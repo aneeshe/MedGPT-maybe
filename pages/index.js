483d8bfb3527e492ef2c9345ffb03f18b45f9693
Jason Zhao <linzhiwei@hotmail.com>
2023-02-01 21:53:28 -0500

    styling updates

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
-import { useState, useRef, useEffect } from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-import ReactMarkdown from 'react-markdown'
-import CircularProgress from '@mui/material/CircularProgress';
+import { useState, useRef, useEffect } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import ReactMarkdown from "react-markdown";
+import CircularProgress from "@mui/material/CircularProgress";
 // import chat from '/api/chatGPT'
 
 export default function Home() {
-
+  const logo =
+    "https://drive.google.com/uc?id=1IGoyc7athVBlqsohJ1-P9WIa_xZ9e0It";
   const [userInput, setUserInput] = useState("");
   const [history, setHistory] = useState([]);
   const [loading, setLoading] = useState(false);
   const [messages, setMessages] = useState([
     {
-      "message": "Hi there! How can I help?",
-      "type": "apiMessage"
-    }
+      message: "Hi there! How can I help?",
+      type: "apiMessage",
+    },
   ]);
 
   const messageListRef = useRef(null);
@@ -34,13 +35,19 @@ export default function Home() {
 
   // Handle errors
   const handleError = () => {
-    setMessages((prevMessages) => [...prevMessages, { "message": "Oops! There seems to be an error. Please try again.", "type": "apiMessage" }]);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        message: "Oops! There seems to be an error. Please try again.",
+        type: "apiMessage",
+      },
+    ]);
     setLoading(false);
     setUserInput("");
-  }
+  };
 
   // Handle form submission
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (userInput.trim() === "") {
@@ -48,13 +55,16 @@ export default function Home() {
     }
 
     setLoading(true);
-    setMessages((prevMessages) => [...prevMessages, { "message": userInput, "type": "userMessage" }]);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { message: userInput, type: "userMessage" },
+    ]);
 
     // Send user question and history to API
-    const response = await fetch("/api/chatGPT", {
+    const response = await fetch("/api/chatGPT.js", {
       method: "POST",
       headers: {
-          "Content-Type": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ question: userInput, history: history }),
     });
@@ -65,15 +75,17 @@ export default function Home() {
     setUserInput("");
 
     // Load the new message
-    setMessages((prevMessages) => [...prevMessages, { "message": data.result, "type": "apiMessage" }]);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { message: data.result, type: "apiMessage" },
+    ]);
     setLoading(false);
-
   };
 
   // Prevent blank submissions and allow for multiline input
   const handleEnter = (e) => {
     if (e.key === "Enter" && userInput) {
-      if(!e.shiftKey && userInput) {
+      if (!e.shiftKey && userInput) {
         handleSubmit(e);
       }
     } else if (e.key === "Enter") {
@@ -84,81 +96,134 @@ export default function Home() {
   // Keep history in sync with messages
   useEffect(() => {
     if (messages.length >= 3) {
-      setHistory([[messages[messages.length - 2].message, messages[messages.length - 1].message]]);
+      setHistory([
+        [
+          messages[messages.length - 2].message,
+          messages[messages.length - 1].message,
+        ],
+      ]);
     }
-    }, [messages])
+  }, [messages]);
 
   return (
     <>
       <Head>
-        <title>MedGPT</title>
+        <img style={{ width: 210, height: 70 }} src={logo} />
         <meta name="description" content="LangChain documentation chatbot" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.topnav}>
-      <div className = {styles.navlogo}>
-    <h1 href="/">MedGPT</h1>
-    </div>
-    <div className = {styles.navlinks}> 
-    <a href="https://github.com/aogara-ds/MedGPT" target="_blank">GitHub</a>
-    </div>
-</div>
-      <main className={styles.main}>
-      <div className = {styles.cloud}>
-        <div ref={messageListRef} className = {styles.messagelist}>
-        {messages.map((message, index) => {
-          return (
-            // The latest message sent by the user will be animated while waiting for a response
-              <div key = {index} className = {message.type === "userMessage" && loading && index === messages.length - 1  ? styles.usermessagewaiting : message.type === "apiMessage" ? styles.apimessage : styles.usermessage}>
-                {/* Display the correct icon depending on the message type */}
-                {message.type === "apiMessage" ? <Image src = "/parroticon.png" alt = "AI" width = "30" height = "30" className = {styles.boticon} priority = {true} /> : <Image src = "/usericon.png" alt = "Me" width = "30" height = "30" className = {styles.usericon} priority = {true} />}
-              <div className = {styles.markdownanswer}>
-                {/* Messages are being rendered in Markdown format */}
-                <ReactMarkdown linkTarget = {"_blank"}>{message.message}</ReactMarkdown>
-                </div>
-              </div>
-          )
-        })}
+        <div className={styles.navlogo}>
+          <h1 href="/"></h1>
         </div>
-            </div>
-           <div className={styles.center}>
-            
-            <div className = {styles.cloudform}>
-           <form onSubmit = {handleSubmit}>
-          <textarea 
-          disabled = {loading}
-          onKeyDown={handleEnter}
-          ref = {textAreaRef}
-          autoFocus = {false}
-          rows = {1}
-          maxLength = {512}
-          type="text" 
-          id="userInput" 
-          name="userInput" 
-          placeholder = {loading? "Waiting for response..." : "Type your question..."}  
-          value = {userInput} 
-          onChange = {e => setUserInput(e.target.value)} 
-          className = {styles.textarea}
-          />
-            <button 
-            type = "submit" 
-            disabled = {loading}
-            className = {styles.generatebutton}
-            >
-            {loading ? <div className = {styles.loadingwheel}><CircularProgress color="inherit" size = {20}/> </div> : 
-            // Send icon SVG in input field
-            <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
-            <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
-          </svg>}
-            </button>
+        <div className={styles.navlinks}>
+          <a href="https://github.com/aogara-ds/MedGPT" target="_blank">
+            GitHub
+          </a>
+        </div>
+      </div>
+      <main className={styles.main}>
+        <div className={styles.cloud}>
+          <div ref={messageListRef} className={styles.messagelist}>
+            {messages.map((message, index) => {
+              return (
+                // The latest message sent by the user will be animated while waiting for a response
+                <div
+                  key={index}
+                  className={
+                    message.type === "userMessage" &&
+                    loading &&
+                    index === messages.length - 1
+                      ? styles.usermessagewaiting
+                      : message.type === "apiMessage"
+                      ? styles.apimessage
+                      : styles.usermessage
+                  }
+                >
+                  {/* Display the correct icon depending on the message type */}
+                  {message.type === "apiMessage" ? (
+                    <Image
+                      src="/parroticon.png"
+                      alt="AI"
+                      width="30"
+                      height="30"
+                      className={styles.boticon}
+                      priority={true}
+                    />
+                  ) : (
+                    <Image
+                      src="/usericon.png"
+                      alt="Me"
+                      width="30"
+                      height="30"
+                      className={styles.usericon}
+                      priority={true}
+                    />
+                  )}
+                  <div className={styles.markdownanswer}>
+                    {/* Messages are being rendered in Markdown format */}
+                    <ReactMarkdown linkTarget={"_blank"}>
+                      {message.message}
+                    </ReactMarkdown>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.center}>
+          <div className={styles.cloudform}>
+            <form onSubmit={handleSubmit}>
+              <textarea
+                disabled={loading}
+                onKeyDown={handleEnter}
+                ref={textAreaRef}
+                autoFocus={false}
+                rows={1}
+                maxLength={512}
+                type="text"
+                id="userInput"
+                name="userInput"
+                placeholder={
+                  loading ? "Waiting for response..." : "Type your question..."
+                }
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
+                className={styles.textarea}
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className={styles.generatebutton}
+              >
+                {loading ? (
+                  <div className={styles.loadingwheel}>
+                    <CircularProgress color="inherit" size={20} />{" "}
+                  </div>
+                ) : (
+                  // Send icon SVG in input field
+                  <svg
+                    viewBox="0 0 20 20"
+                    className={styles.svgicon}
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                  </svg>
+                )}
+              </button>
             </form>
-            </div>
-            <div className = {styles.footer}>
-            <a href="https://langchain.readthedocs.io/en/latest/" target="_blank">Built on LangChain</a>
-            </div>
+          </div>
+          <div className={styles.footer}>
+            <a
+              href="https://langchain.readthedocs.io/en/latest/"
+              target="_blank"
+            >
+              Built on LangChain
+            </a>
+          </div>
         </div>
       </main>
     </>
-  )
+  );
 }
