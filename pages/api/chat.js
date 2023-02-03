efdb2e1f97441f5fcf0ae4ec9418bda1a0cef624
aneeshe <aneesh.v.edara@gmail.com>
2023-02-03 00:11:24 -0500

    working

---
+++ pages/api/chat.js
@@ -0,0 +1,29 @@
+export default async function (req, res) {
+
+  const url = "https://api-inference.huggingface.co/models/stanford-crfm/pubmedgpt";
+
+  const api_token = process.env.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;
+  process.env
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {"Authorization": `Bearer ${api_token}`},
+    body: JSON.stringify({
+      "inputs": {
+        "past_user_inputs": null,
+        "generated_responses": null,
+        "text": "Photosynthesis is "
+      },
+      "options": {
+        "use_gpu": true,
+        "use_cache": false,
+        "wait_for_model": false
+      }
+    })
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+
+  res.status(200).json({ result: data })
+}
